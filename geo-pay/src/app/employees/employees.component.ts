8abb02a17276f921b2945cc21ceb1a46561835f6
Barbez <bkamau032@gmail.com>
2024-05-27 15:36:31 +0300

    added search functionality

--- geo-pay/src/app/employees/employees.component.ts
+++ geo-pay/src/app/employees/employees.component.ts
@@ -4,27 +4,76 @@ import { NgFor,NgIf } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { AuthServiceService } from '../auth-service.service';
 import { HttpClient } from '@angular/common/http';
+import { Detail, Employees } from '../employee';
+import { AddDetailsFormComponent } from '../add-details-form/add-details-form.component';
+import { MatDialog, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 
 @Component({
   selector: 'app-employees',
   standalone: true,
-  imports: [NgFor,NgIf,MatCardModule],
+  imports: [NgFor, NgIf, MatCardModule, FormsModule,MatIconModule,MatInputModule,MatFormFieldModule,MatIconModule],
   templateUrl: './employees.component.html',
-  styleUrl: './employees.component.scss'
+  styleUrl: './employees.component.scss',
 })
-export class EmployeesComponent implements OnInit{
+export class EmployeesComponent implements OnInit {
   authService = inject(AuthServiceService);
   http = inject(HttpClient);
+  searchTerm: string = '';
 
-employees: string[] = [];
+  employees!: Employees;
+  filteredEmployees: Detail[] = [];
 
-constructor(
+
+  constructor(
     private employeesService: EmployeesService,
-  ) {  }
+    private dialog: MatDialog
+  ) {}
 
-ngOnInit(): void {
-      this.employees = this.employeesService.getEmployees();
+  ngOnInit(): void {
+    this.viewAllEmployees();
+    // this.removeEmployee();
   }
 
+  viewAllEmployees(): void {
+    this.employeesService.getEmployees().subscribe((data: Employees) => {
+      this.employees = data;
+      this.filteredEmployees = this.employees.details;
+    });
+  }
+
+  searchEmployees(): void {
+    const term = this.searchTerm.toLowerCase();
+    this.filteredEmployees = this.employees.details.filter(employee => 
+      employee.firstName.toLowerCase().includes(term) ||
+      employee.lastName.toLowerCase().includes(term) ||
+      employee.department.toLowerCase().includes(term)
+    );
+  }
+
+  removeEmployee(id: string): void {
+    // this.employeesService.removeEmployee(this.employees.id).subscribe(() => {
+    // kufilter out the removed employee from the list
+    this.employees.details = this.employees.details.filter(
+      (employee) => employee.id !== id
+    );
+    this.filteredEmployees = this.filteredEmployees.filter((employee) => employee.id !== id);
+  }
+
+ 
+  
+
+  openAddEmployeeDialog(): void {
+    this.dialog.open(AddDetailsFormComponent);
+  }
 }
+
+  
+
+
+
+
--- geo-pay/src/app/employees/employees.service.ts
+++ geo-pay/src/app/employees/employees.service.ts
@@ -1,5 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Employees } from '../employee';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,10 +13,109 @@ export class EmployeesService {
 
    // readonly #http = inject(HttpClient)
   // private baseUrl = `/request`
-  private employees: string[] = ['Employee 1', 'Employee 2', 'Employee 3'];
+  private employees: Employees = 
+  
+    {
+      "count":10,
+         "next":null,
+         "previous":null,
+         "details":[
+             {
+                 "id": "1234567890",
+                 "firstName": "John",
+                 "lastName": "Doe",
+                 "email": "john.doe@example.com",
+                 "phoneNumber": "+1234567890",
+                 "dob": "1990-01-01",
+                 "date_created": "2024/05/23",
+                 "county": "Nairobi",
+                 "subCounty": "Lang'ata",
+                 "nationalId": "1234567890",
+                 "department": "backend",
+                 "documents": [
+                     {
+                         "name": "ID Copy",
+                         "documentId": "abc123"
+                     },
+                     {
+                      "name": "Good Conduct",
+                      "documentId": "abc123"
+                    }, 
+                    {
+                      "name": "Birth Certificate",
+                      "documentId": "abc123"
+                    }
+                 ],
+                 "equipments": [
+                     {
+                         "name": "Laptop",
+                         "description": "Dell Latitude"
+                     }
+                 ]
+             },
+             {
+              "id": "1234567891",
+              "firstName": "Jmaes",
+              "lastName": "Doe",
+              "email": "james.doe@example.com",
+              "phoneNumber": "+1234567890",
+              "dob": "1993-02-01",
+              "date_created": "2024/05/24",
+              "county": "Samburu",
+              "subCounty": "Samburu",
+              "nationalId": "12345678",
+              "department": "frontend",
+
+              "documents": [
+                  {
+                      "name": "ID Copy",
+                      "documentId": "abc123"
+                  }
+              ],
+              "equipments": [
+                  {
+                      "name": "Desktop",
+                      "description": "Dell Latitude"
+                  }
+              ]
+          },
+          {
+            "id": "1234567892",
+            "firstName": "Jane",
+            "lastName": "Doe",
+            "email": "jane.doe@example.com",
+            "phoneNumber": "+123456789",
+            "dob": "1995-01-08",
+            "date_created": "2024/05/23",
+            "county": "Mombasa",
+            "subCounty": "Tudor",
+            "nationalId": "1234567890",
+            "department": "BA",
+
+            "documents": [
+                {
+                    "name": "Birth Certificate",
+                    "documentId": "abc123"
+                }
+            ],
+            "equipments": [
+                {
+                    "name": "Laptop",
+                    "description": "Dell Latitude"
+                }
+            ]
+        }
+         ]
+ }
+  
 
 
-  getEmployees() {
-    return this.employees
+  getEmployees(): Observable<Employees>{
+    return of (this.employees);
+  }
+
+  removeEmployee(id: number) {
+    // return this.http.delete<void>(`/api/employees/${employeeId}`);
+    console.log('employee id', this.employees.details);
   }
 }
